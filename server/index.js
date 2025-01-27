20f67d9dc25d095377f0d3ecebbfcfa9c198dbdc
Caitlin Stevens <caity.stevens6106@gmail.com>
2025-01-27 15:05:40 +0000

    made blogpost class

--- server/index.js
+++ server/index.js
@@ -9,13 +9,34 @@ app.listen(
 )
 
 app.get('/', (req, res) => {
-    res.send();
+    res.send("bonk");
 });
 
 app.get('/api/posts/:id', (req, res) => {
     res.send();
 })
 
+all_posts = [BlogPost];
+
+class BlogPost {
+    title = String
+    description = String
+    author = String
+    reading_time = Number
+    body = String
+    date = Date
+    comments = [String]
+
+    constructor(){
+        title = this.title;
+        description = this.description;
+        author = this.author;
+        reading_time = this.reading_time;
+        body = this.body;
+        date = this.date;
+        comments = this.comments;
+    }
+}
 
 
 /* GET = retrieve data, POST = write new data, DELETE = delete junk, PUT = write update data
