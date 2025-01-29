fd936cb2fe2bdcfae90f99001cc2ab8c8f71fb25
Caitlin Stevens <caity.stevens6106@gmail.com>
2025-01-29 18:15:01 +0000

    i think it works now

--- server/index.js
+++ server/index.js
@@ -1,42 +1,115 @@
-const app = require('express')();
-const http = require('http');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 
+const corsOptions = {
+    origin: "*"
+};
 
-const port = process.env.PORT || 8080;
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+all_posts = [];
+all_comments = [];
+
+fs.stat('storedposts.json', (err, _) => {
+    if (err == null) {
+        fs.readFile('storedposts.json', (err, data) => {
+            if (err == null) {
+                all_posts = JSON.parse(data.toString());
+            }
+        });
+    }
+})
+
+fs.stat('storedcomments.json', (err, _) => {
+    if (err == null) {
+        fs.readFile('storedcomments.json', (err, data) => {
+            if (err == null) {
+                all_comments = JSON.parse(data.toString());
+            }
+        });
+    }
+})
+
+const port = 8080;
 app.listen(
     port,
     () => console.log(`it's alive on http://localhost:${port}`)
 )
 
 app.get('/', (req, res) => {
-    res.send("bonk");
+    res.status(200).sendFile(path.join(__dirname, 'homepage.html'));
 });
 
-app.get('/api/posts/:id', (req, res) => {
-    res.send();
+app.get('/css/styles.css', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'styles.css'));
 })
 
-all_posts = [BlogPost];
+app.get('/api/posts/:id', (req, res) => {
+    res.status(200).send();
+});
 
-class BlogPost {
-    title = String
-    description = String
-    author = String
-    reading_time = Number
-    body = String
-    date = Date
-    comments = [String]
+app.get('/api/home', (req, res) => {
+    console.log(all_posts);
+    res.status(200).json(all_posts);
+});
 
-    constructor(){
-        title = this.title;
-        description = this.description;
-        author = this.author;
-        reading_time = this.reading_time;
-        body = this.body;
-        date = this.date;
-        comments = this.comments;
+app.post('/api/admin/create', (req, res) => {
+    req.body.comments = [];
+    req.body.date = Date.now();
+    req.body.id = all_posts.length;
+    all_posts.push(req.body);
+    fs.writeFile('storedposts.json', JSON.stringify(all_posts), () => {});
+    res.sendStatus(200);
+});
+
+app.get('/api/comments/get', (req, res) => {
+    let returnComment;
+    all_comments.forEach((comment) => {
+        if (comment.id.toString() == req.query.commentId) {
+            returnComment = comment;
+        }
+    })
+    if (returnComment) {
+        res.status(200).json(returnComment)
+    } else {
+        res.sendStatus(404)
     }
+});
+
+/*{
+    postId: number
+    comment: string
 }
+*/
+
+app.post('/api/comments/post', (req, res) => {
+    if (all_posts.length > req.body.postId) {
+        req.body.id = all_comments.length;
+        let post;
+        all_posts.forEach((testPost) => {
+            if (req.body.postId === testPost.id) {
+                post = testPost;
+            }
+        })
+        if (post != null) {
+            post.comments.push(req.body.id);
+            all_comments.push(req.body);
+            fs.writeFile('storedposts.json', JSON.stringify(all_posts), () => {});
+            fs.writeFile('storedcomments.json', JSON.stringify(all_comments), () => {});
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(404);
+        }
+    } else {
+        res.sendStatus(404);
+    }
+});
+
 
 
 /* GET = retrieve data, POST = write new data, DELETE = delete junk, PUT = write update data
